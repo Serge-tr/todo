66ac705008ad3f63b2df864a7850c8298b4914b0
serge-teretshenko <teretshenko.s@gmail.com>
2017-04-28 13:13:48 +0300

    Todo with Redux

---
+++ app/reducers/reducers.jsx
@@ -0,0 +1,33 @@
+export let searchTextReducer = (state = '', action) => {
+    switch (action.type) {
+        case 'SET_SEARCH_TEXT':
+            return action.searchText;
+        default:
+            return state;
+    }
+};
+
+export let showCompletedReducer = (state = false, action) => {
+    switch (action.type) {
+        case 'TOGGLE_SHOW_COMPLETED':
+            return !state;
+        default:
+            return state
+    }
+};
+
+export let todosReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [
+                ...state, 
+                {
+                    id: uuid(),
+                    text: action.text,
+                    completed: false,
+                    createdAt: moment().unix(),
+                    completedAt: undefined
+                }
+            ];
+    }
+};
